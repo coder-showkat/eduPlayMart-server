c728348cbf3f4961183cc8b612150bba95a055d8
coder-showkat <showkat.coder@gmail.com>
2023-05-18 13:10:10 +0600

    basic setup

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongodb
+const uri = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(uri);
+
+const run = async () => {
+  try {
+    const Toys = client.db("EduKit").collection("toys");
+
+    // get all toys
+    app.get("/api/toys", async () => {
+      try {
+        const skip = Number(req.query.page) - 1 || 0;
+        const limit = 20;
+        const toys = await Toys.find().skip(skip).limit(limit).toArray();
+        res.send(toys);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
+
+    // get toys by category
+    app.get("/api/toys/:subCategory", async (req, res) => {
+        try {
+            const subCategory = req.params.subCategory;
+            const toys = await Toys.find({subCategory}).toArray();
+            res.send(toys);
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// home route
+app.get("/", (req, res) => {
+  res.send("<h1 style='text-align: center;'>Welcome to EduKit Server</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
