00a9696c476c2c819013a3e5c98eb22f57a6a96d
coder-showkat <showkat.coder@gmail.com>
2023-05-20 22:09:58 +0600

    adding get blogs and get blog by id api

--- index.js
+++ index.js
@@ -11,22 +11,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 // verify jwt token
 const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization.split(" ")[1];
-    if (!token) {
-        res.status(401).send({error: "unauthorized user"});
-        return;
-    }
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    if (decoded) {
-        req.decoded = decoded;
-        next();
-    } else {
-        res.status(401).send({error: "unauthorized user"});
-    }
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) {
+    res.status(401).send({ error: "unauthorized user" });
+    return;
+  }
+  const decoded = jwt.verify(token, process.env.SECRET_KEY);
+  if (decoded) {
+    req.decoded = decoded;
+    next();
+  } else {
+    res.status(401).send({ error: "unauthorized user" });
+  }
 };
 
 // mongodb
@@ -35,13 +33,8 @@ const client = new MongoClient(uri);
 
 const run = async () => {
   try {
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
     const Toys = client.db("EduPlayMart").collection("toys");
-
-    // create text index
-    await Toys.createIndex({name: "text"});
-
+    const Blogs = client.db("EduPlayMart").collection("blogs");
 
     // get all toys
     app.get("/api/toys", async (req, res) => {
@@ -51,7 +44,9 @@ const run = async () => {
         const skip = (page - 1) * limit;
         const sort = req.query.sort;
         const search = req.query.search || "";
-  
+
+        // create text index
+        await Toys.createIndex({ name: "text" });
         let toys;
         switch (sort) {
           case "price-ascending": {
@@ -146,7 +141,9 @@ const run = async () => {
     // generate jwt access token
     app.post("/api/jwt", async (req, res) => {
       try {
-        const token = jwt.sign(req.body, process.env.SECRET_KEY, {expiresIn: "2d"});
+        const token = jwt.sign(req.body, process.env.SECRET_KEY, {
+          expiresIn: "2d",
+        });
         res.send({ token: "Bearer " + token });
       } catch (error) {
         res.status(500).send({ error: error.message });
@@ -155,58 +152,84 @@ const run = async () => {
 
     // get toys by seller jwt token
     app.get("/api/seller/toys", verifyToken, async (req, res) => {
-        try {
-            const {email} = req.decoded;
-            const sort = req.query.sort;
-            const result = Toys.find({sellerEmail: email});
-            const sortedResult = sort === "price-ascending" ? result.sort({price: 1}) : sort === "price-descending" ? result.sort({price: -1}) : result;
-            const toys = await sortedResult.toArray();
-            res.send(toys);
-        } catch (error) {
-            res.status(500).send({ error: error.message });
-        }
-    })
+      try {
+        const { email } = req.decoded;
+        const sort = req.query.sort;
+        const result = Toys.find({ sellerEmail: email });
+        const sortedResult =
+          sort === "price-ascending"
+            ? result.sort({ price: 1 })
+            : sort === "price-descending"
+            ? result.sort({ price: -1 })
+            : result;
+        const toys = await sortedResult.toArray();
+        res.send(toys);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
 
     // add a toy
     app.post("/api/seller/toys", verifyToken, async (req, res) => {
-        try {
-            const {email} = req.decoded;
-            if (email !== req.body.sellerEmail) {
-                res.status(403).send({error: "bad auth"});
-                return;
-            }
-            const result = await Toys.insertOne(req.body);
-            res.status(201).send(result);
-        } catch (error) {
-            res.status(500).send({ error: error.message });
+      try {
+        const { email } = req.decoded;
+        if (email !== req.body.sellerEmail) {
+          res.status(403).send({ error: "bad auth" });
+          return;
         }
-    })
-
+        const result = await Toys.insertOne(req.body);
+        res.status(201).send(result);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
 
     // update a toy
     app.put("/api/seller/toys/:id", verifyToken, async (req, res) => {
-        try {
-            const _id = new ObjectId(req.params.id);
-            const {price, availableQty, details} = req.body;
-            const result = await Toys.updateOne({_id}, {$set: {price, availableQty, details}});
-            res.send(result);
-        } catch (error) {
-            res.status(500).send({ error: error.message });
-        }
-    })
+      try {
+        const _id = new ObjectId(req.params.id);
+        const { price, availableQty, details } = req.body;
+        const result = await Toys.updateOne(
+          { _id },
+          { $set: { price, availableQty, details } }
+        );
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
 
     // delete a toy
     app.delete("/api/seller/toys/:id", verifyToken, async (req, res) => {
-        try {
-            const _id = new ObjectId(req.params.id);
-            const result = await Toys.deleteOne({_id});
-            res.send(result);
-        } catch (error) {
-            res.status(500).send({ error: error.message });
-        }
-    })
+      try {
+        const _id = new ObjectId(req.params.id);
+        const result = await Toys.deleteOne({ _id });
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
 
+    // get all blogs
+    app.get("/api/blogs", async (req, res) => {
+      try {
+        const blogs = await Blogs.find().toArray();
+        res.send(blogs);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
 
+    // get get blog by id
+    app.get("/api/blogs/:id", async (req, res) => {
+      try {
+        const _id = new ObjectId(req.params.id);
+        const blog = await Blogs.findOne({ _id });
+        res.send(blog);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
   } catch (error) {
     console.log(error);
   }
